5782ec5a111f84cf876b5e0410e3cf116cfc19d8
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-06-24 19:15:48 -0700

    Time: 71 ms (55.03%), Space: 14.5 MB (34.42%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,15 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-#       compare the length of both strings
+#         2 strategies: hash table or sort
+
+        # return sorted(s) == sorted(t) - sorting solution
+    
         if len(s) != len(t):
             return False
-#       implement a hashmap for both strings
+        
+        
+#         create hash table
         countS, countT = {}, {}
-#       adding to hashmap/count
+        
+#         add characters to hashtable
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i],0)
             countT[t[i]] = 1 + countT.get(t[i],0)
-#       compare both hashmap
+        
+#         compare both strings in hashtable
         for c in countS:
             if countS[c] != countT.get(c,0):
                 return False
