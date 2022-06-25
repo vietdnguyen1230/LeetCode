b7471bf18b28f4454de9e9f220012b7c2a905c46
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-06-25 13:44:57 -0700

    Time: 1042 ms (49.50%), Space: 27.5 MB (54.29%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,11 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        
+# use 2 pointers
         res = 0
         l, r = 0, len(height) - 1
         
         while l < r:
-            area = (r-l) * min(height[l], height[r])
-            res = max(res, area)
+            area = (r - l) * min(height[l], height[r])
+            res = max(area, res)
             
             if height[l] < height[r]:
                 l += 1
@@ -15,3 +17,6 @@ class Solution:
                 l += 1
         
         return res
+    
+# Time: O(n)
+# Space: O(n)
