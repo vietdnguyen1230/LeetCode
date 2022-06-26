8e6b8ce8bfb63d28ef8a387ea8816a734426ddc7
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-06-26 12:53:37 -0700

    Time: 687 ms (95.00%), Space: 18.1 MB (71.97%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,18 +1,14 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
-# sort the array
-# time O (nlogn) + O (n^2)  = O(n^2)
-# Space O(n)
-
         res = []
-# sort the array
         nums.sort()
         
         for i, n in enumerate(nums):
+# don't want to reuse same variable twice
             if i > 0 and n == nums[i-1]:
                 continue
-# 2 pointers
+
+# 2 pointer
             l, r = i + 1, len(nums) - 1
             while l < r:
                 threeSum = n + nums[l] + nums[r]
@@ -25,4 +21,5 @@ class Solution:
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
+        
         return res
