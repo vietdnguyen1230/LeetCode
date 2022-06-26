8a8046634364e455aebad8c1aad46f36d66cb24e
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-06-26 07:05:44 -0700

    Time: 96 ms (60.80%), Space: 15.2 MB (24.13%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,16 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#     use hashmap
         prevMap = {}
         
         for i, n in enumerate(nums):
-#     find difference
             diff = target - n
-#     if difference is already in hashmap
             if diff in prevMap:
                 return [prevMap[diff], i]
-#     otherwise add to hashmap
             else:
                 prevMap[n] = i
+            
         
         return
