a6b0687cc58dd31d59278f42825415a3420d6980
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-06-30 07:06:07 -0700

    Time: 397 ms (11.06%), Space: 13.9 MB (57.46%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,17 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+# sliding window
+# 2 pointers
         count = {}
-        l = 0
         res = 0
         
+        l = 0
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            
+# store in hash table
+            count[s[r]] = 1 + count.get(s[r],0)
+# number of replaceable cannot be greather than k 
             while (r - l + 1) - max(count.values()) > k:
-                
+# shift left pointer
                 count[s[l]] -= 1
                 l += 1
-                
+            
             res = max(res, r - l + 1)
         
         return res
