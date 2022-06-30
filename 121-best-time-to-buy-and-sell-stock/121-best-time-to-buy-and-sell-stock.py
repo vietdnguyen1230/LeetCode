93a4065b667bdea235c73e53f34e817e2b81089a
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-06-30 07:00:51 -0700

    Time: 2298 ms (5.28%), Space: 25 MB (85.33%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0,0
-        res = 0
-        
+# set two pointers
+        l, r = 0, 0
+        maxProfit = 0
+# while within range
         while r < len(prices):
             if prices[l] < prices[r]:
                 profit = prices[r] - prices[l]
-                res = max(profit, res)
-            
+                maxProfit = max(maxProfit, profit)
+# move the pointers
             else:
                 l = r
             r += 1
         
-        return res
-            
+        return maxProfit
