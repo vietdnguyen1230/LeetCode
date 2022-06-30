345b5789d4c9f0c94c738e56fa5c068aba2c41ab
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-06-30 07:16:16 -0700

    Time: 130 ms (52.11%), Space: 14.1 MB (31.22%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,15 +1,12 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-# sliding window
-# hashmap/arrays
-# matches variable
         if len(s1) > len(s2): return False
-    
+        
         s1Count, s2Count = [0] * 26, [0] * 26
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord("a")] +=1
-            s2Count[ord(s2[i]) - ord("a")] +=1
-        
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+    
         matches = 0
         for i in range(26):
             matches += (1 if s1Count[i] == s2Count[i] else 0)
@@ -32,4 +29,5 @@ class Solution:
             elif s1Count[index] - 1 == s2Count[index]:
                 matches -= 1
             l += 1
+        
         return matches == 26
