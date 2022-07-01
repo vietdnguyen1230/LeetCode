ccc0d1980c739432db40afd444cafd8b0a0e3fe7
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-07-01 07:08:03 -0700

    Time: 1381 ms (47.00%), Space: 18 MB (72.19%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -7,21 +7,22 @@ class Solution:
 # don't want to reuse same variable twice
             if i > 0 and n == nums[i-1]:
                 continue
-
-# 2 pointer
-            l, r = i + 1, len(nums) - 1
+        
+            l,r = i + 1, len(nums) - 1
             while l < r:
                 threeSum = n + nums[l] + nums[r]
-                if threeSum > 0:
+                if threeSum > 0: 
                     r -= 1
                 elif threeSum < 0:
                     l += 1
-                else: 
+                else:
                     res.append([n, nums[l], nums[r]])
-# update pointer
                     l += 1
-# don't reuse same variable
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
         
         return res
+    
+    
+    
+
