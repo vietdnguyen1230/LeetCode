47437301dab25501f2fdfb3de92a41dde20f3f56
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-07-02 11:18:43 -0700

    Time: 2725 ms (44.91%), Space: 30.1 MB (49.27%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -5,18 +5,18 @@ class Solution:
         l = r = 0
         
         while r < len(nums):
-# pop smaller values from q
             while q and nums[q[-1]] < nums[r]:
                 q.pop()
             q.append(r)
-
-# remove left value from window
+            
             if l > q[0]:
                 q.popleft()
-            
+                
             if (r + 1) >= k:
                 output.append(nums[q[0]])
                 l += 1
             r += 1
         
         return output
+    
+    
