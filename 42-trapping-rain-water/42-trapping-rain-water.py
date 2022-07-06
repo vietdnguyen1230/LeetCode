d51e7cb7cb9c27fcc82bd9ca6e910bfa468519c6
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-07-06 06:35:33 -0700

    Time: 183 ms (52.13%), Space: 15.9 MB (69.26%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,9 +1,9 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
+        if not height: 
             return 0
-        
-        l, r = 0, len(height) - 1
+# 2 pointers
+        l,r = 0, len(height) - 1
         leftMax, rightMax = height[l], height[r]
         res = 0
         
@@ -16,5 +16,7 @@ class Solution:
                 r -= 1
                 rightMax = max(rightMax, height[r])
                 res += rightMax - height[r]
-            
+        
         return res
+    
+# Time: O(1)
