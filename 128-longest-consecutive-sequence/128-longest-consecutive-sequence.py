1eb09a6962cf2e692a523251eb5eb70d08c389c4
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-07-16 15:50:12 -0700

    Time: 2262 ms (28.93%), Space: 28.9 MB (20.39%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -4,10 +4,11 @@ class Solution:
         longest = 0
         
         for n in nums:
-            # check if its the start of a sequence
-            if (n - 1) not in numSet:
-                length = 1
+#         check if is start of sequence
+            if (n - 1) not in numSet: 
+                length = 0
                 while (n + length) in numSet:
                     length += 1
-                longest = max(length, longest)
+                longest = max(longest,length)
+        
         return longest
