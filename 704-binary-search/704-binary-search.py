c7cb6aa36cc3ad6e1524df05e9733bbb150939f7
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-07-21 06:34:16 -0700

    Time: 458 ms (18.09%), Space: 15.4 MB (97.45%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -3,7 +3,7 @@ class Solution:
         l,r = 0, len(nums) - 1
         
         while l <= r:
-            m = (l + r) // 2
+            m = (l + r ) // 2
             if nums[m] > target:
                 r = m - 1
             elif nums[m] < target:
@@ -12,3 +12,7 @@ class Solution:
                 return m
         
         return -1
+    
+                
+            
+            
