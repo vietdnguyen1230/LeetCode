1f28f85930e65fb3e467ebba00d78361dc2411d2
vietdnguyen1230 <101231627+vietdnguyen1230@users.noreply.github.com>
2022-08-31 07:43:07 -0700

    Time: 36 ms (96.38%), Space: 13.9 MB (32.59%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -8,7 +8,7 @@ class Solution:
         dummy = ListNode()
         tail = dummy
         
-        while list1 and list2:
+        while list1 and list2: 
             if list1.val < list2.val:
                 tail.next = list1
                 list1 = list1.next
@@ -16,10 +16,10 @@ class Solution:
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next
-        
+            
         if list1:
             tail.next = list1
         elif list2:
             tail.next = list2
-
+        
         return dummy.next
